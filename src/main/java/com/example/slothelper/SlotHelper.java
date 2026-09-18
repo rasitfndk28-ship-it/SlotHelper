3813a6cc432eba38b068a06d3fa9742f85883fbf
rasitfndk28-ship-it <rasitfndk.28@gmail.com>
2026-09-18 13:27:38 +0300

    Create SlotHelper.java

---
+++ src/main/java/com/example/slothelper/SlotHelper.java
@@ -0,0 +1,11 @@
+package com.example.slothelper;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SlotHelper.MOD_ID)
+public class SlotHelper {
+    public static final String MOD_ID = "slothelper";
+
+    public SlotHelper() {
+    }
+}
